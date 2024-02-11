54ebc1fa8b531e32ffb94b5d20f482733d26899d
deniz <deniz@MSI>
2024-02-11 14:27:07 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Person person1 = new Person();
+            person1.FirstName = "Deniz";
+            person1.LastName = "Yalçınkaya";
+            person1.DateOfBirthYear = 1999;
+            person1.NationalIdentity = 123;
+
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+
+
+        }
+
+
+
+      
+
+
+        
+    }
+
+    
+
+}
